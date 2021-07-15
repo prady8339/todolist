ff304f31fb6a1c5e03f90c9148968a909afc5618
prady8339 <76277136+prady8339@users.noreply.github.com>
2021-07-15 19:59:53 +0530

    toLocateDateString update..

--- app.js
+++ app.js
@@ -8,25 +8,9 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get("/",function(req,res){
-
-    var today = new Date();
-    var day ="";
-    var currentDay = today.getDay();
-if(currentDay == 0)
-day = "Sunday";
-else if(currentDay == 1)
-day = "Monday"
-else if(currentDay == 2)
-day = "Tuesday"
-else if(currentDay == 3)
-day = "Wednesday"
-else if(currentDay == 4)
-day = "Thursday"
-else if(currentDay == 5)
-day = "Friday"
-else if(currentDay == 6)
-day = "Saturday"
-
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var today  = new Date();
+var day = today.toLocaleDateString("en-US",options)
 res.render("list",{ kindOfDay : day}); 
 });
 
