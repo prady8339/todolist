d31f15879aa70dfdf5f00858295048e40d6c9db0
prady8339 <76277136+prady8339@users.noreply.github.com>
2021-07-16 15:25:14 +0530

    add templeteing

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items=['buy food','cook food','eat food'];
 
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     var today  = new Date();
 var day = today.toLocaleDateString("en-US",options)
-res.render("list",{ kindOfDay : day}); 
+
+ res.render("list",{ kindOfDay : day , newItem : items}); 
 });
 
+app.post("/",function(req,res){
+    var item = req.body.newItem;
+    console.log(items);
+    items.push(item);
+  res.redirect("/");
+
+});
 app.listen(3000,function(){
     console.log("server running on port 30000");
+    
 });
